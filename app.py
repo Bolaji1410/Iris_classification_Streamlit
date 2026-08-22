3d7646c43cf581f7c76171fc95c6c371f1606d3b
Bolaji <bolajibello052@gmail.com>
2026-08-22 06:13:04 +0100

    first commit

---
+++ app.py
@@ -0,0 +1,7 @@
+import streamlit as st
+
+st.title("🌸 Iris Flower Classifier")
+
+st.write("Welcome this is for Educational Purposes!")
+
+st.write("This app will predict the species of an Iris flower.")
